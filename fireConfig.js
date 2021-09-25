e0cf936756570a7412c8e2a85bcd35192bc8cff1
Priya Tarkar <priya.gla_cs19@gla.ac.in>
2021-09-25 23:31:45 +0530

    first commit

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig={
+    apiKey: "AIzaSyCr5-CTbCM98drKzox2LurZH9Oh2gZi7H8",
+    authDomain: "myfireprowithcrud-10aff.firebaseapp.com",
+    databaseURL: "https://myfireprowithcrud-10aff-default-rtdb.firebaseio.com",
+    projectId: "myfireprowithcrud-10aff",
+    storageBucket: "myfireprowithcrud-10aff.appspot.com",
+    messagingSenderId: "395987660091",
+    appId: "1:395987660091:web:3f9d45dc6386b9ccd99b1e"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
